8a75d435157dc1dbfcdc2babd2d273e5ccee6f27
Toby Sullivan <tobyjsullivan@gmail.com>
2016-06-05 12:13:46 -0700

    Initial commit of dummy endpoint

---
+++ routes/routes.js
@@ -0,0 +1,52 @@
+'use strict'
+
+var express = require('express');
+const querystring = require('querystring');
+const url = require('url');
+const urljoin = require('url-join')
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/authenticate', function(req, res, next) {
+  var callback = null;
+  if (!req.query.callback) {
+    res.send('You must supply a callback URL.');
+    return;
+  }
+  callback = req.query.callback;
+
+  var err = null;
+  if (req.query.e){
+    err = req.query.e;
+  }
+
+  res.render('authenticate', {callback: callback, error: err});
+});
+
+router.post('/authenticate', function(req, res) {
+  var cb = req.body.callback;
+
+  if (!req.body.username) {
+    authDisplayError('You must provide a username.', cb, res);
+    return;
+  }
+
+  var authToken = 'placeholder_auth_token';
+
+  // Merge the callback URL with the token query param
+  var cbUrl = url.parse(cb, true);
+  // TODO: urljoin really sucks. Swap it out for a smarter URL builder if we need to do this for real auth.
+  var redUrl = urljoin(cbUrl.href, '?'+querystring.stringify({TOKEN: authToken}));
+
+  console.log(redUrl);
+  res.redirect(redUrl);
+});
+
+function authDisplayError(err, cb, res) {
+  var qs = querystring.stringify({ e: err, callback: cb });
+
+  res.redirect('/authenticate?'+qs);
+}
+
+module.exports = router;
